85253ac2d04831ada15e2a7e5cf958f7216d2131
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-07-31 14:55:30 +0300

    use UIComponent abstract class

---
+++ src/include/Crosshair.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "VertexArray.h"
+#include "VertexBuffer.h"
+#include "UIComponent.h"
+#include <array>
+
+class Crosshair : public UIComponent 
+{
+public:
+	Crosshair();
+
+	void render() override;
+private:
+	std::array<float, 8> m_vertices;
+
+	VertexArray m_VAO;
+	VertexBuffer m_VBO;
+};
