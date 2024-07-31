5df29b7ab73f0dc538a73104cbb0130c4d46b4ba
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-07-31 14:56:39 +0300

    added setVec2 function

--- src/include/Shader.h
+++ src/include/Shader.h
@@ -28,6 +28,7 @@ public:
     void setFloat(const std::string& name, float value) const;
     void setVec4(const std::string& name, float x, float y, float z, float w) const;
     void setVec3(const std::string& name, float x, float y, float z) const;
+    void setVec2(const std::string& name, float x, float y) const;
     void setVec4(const std::string& name, float value) const;
     void setMat4(const std::string& name, const glm::mat4& mat) const;
 
