76787b8af86b32f4126c2f12ca22247bcddeaf24
Jeleriu Antonio Emanuel <152036671+AntoEnterpriseAJ@users.noreply.github.com>
2024-08-10 14:40:15 +0300

    Update Block.h

--- src/include/Block.h
+++ src/include/Block.h
@@ -42,9 +42,9 @@ public:
     void setType(Block::Type type);
 
     static constexpr std::array<float, 18> s_faceNormals = {
-        0.0f,  0.0f,  1.0f, // FRONT
+        	0.0f,  0.0f,  1.0f, // FRONT
 		0.0f,  0.0f, -1.0f, // BACK
-	   -1.0f,  0.0f,  0.0f, // LEFT
+	       -1.0f,  0.0f,  0.0f, // LEFT
 		1.0f,  0.0f,  0.0f, // RIGHT
 		0.0f,  1.0f,  0.0f, // TOP
 		0.0f, -1.0f,  0.0f, // BOTTOM
